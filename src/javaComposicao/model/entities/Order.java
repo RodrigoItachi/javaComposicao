27a80c5b3a362c7b85cd14c2a0ab0d9a9886e930
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-25 13:49:20 -0300

    começando a implementação da classe Order

--- src/javaComposicao/model/entities/Order.java
+++ src/javaComposicao/model/entities/Order.java
@@ -1,5 +1,32 @@
 package javaComposicao.model.entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javaComposicao.model.enuns.OrderStatus;
+
 public class Order {
-	
+	private static SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	private Date moment;
+	private OrderStatus status;
+
+	public Order() {
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
 }
