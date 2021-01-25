6fc7c9d87d3e9643d1ec1747c3f4ca2e049a02a9
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-25 14:15:35 -0300

    foi add dois métodos para poder acessar a lista

--- src/javaComposicao/model/entities/Order.java
+++ src/javaComposicao/model/entities/Order.java
@@ -48,4 +48,13 @@ public class Order {
 		this.client = client;
 	}
 
+	public void addItem(OrderItem item) {
+		this.items.add(item);
+	}
+
+	public void removeItem(OrderItem item) {
+		this.items.remove(item);
+	}
+	
+
 }
