8b0796e52b41972f12a1b042d2fdf4c26ebace59
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-25 21:06:06 -0300

    Preparando a classe Teste para os testes

--- src/javaComposicao/application/Teste.java
+++ src/javaComposicao/application/Teste.java
@@ -1,9 +1,26 @@
 package javaComposicao.application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import javaComposicao.model.entities.Client;
+import javaComposicao.model.entities.Order;
+import javaComposicao.model.entities.OrderItem;
+import javaComposicao.model.entities.Product;
+import javaComposicao.model.enuns.OrderStatus;
+
 public class Teste {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner leia = new Scanner(System.in);
 
+		
+
+		leia.close();
 	}
 
 }
